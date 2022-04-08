c24a534fbed4526f7fc51804b030ead4d1e25136
congphamtienthanh <congphamtienthanh@gmail.com>
2022-04-08 23:50:47 +0700

    create data

---
+++ database/migrations/2022_04_08_155920_create_admins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 50)->unique();
+            $table->string('image', 100)->default("avatar.jpg");
+            $table->text('description');
+            $table->float('income')->default(0.0);
+            $table->string('password', 70);
+            $table->string('token', 100);
+            $table->integer('lever')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
