52e85819759500ea5c159150d9db3f8ae493053a
congphamtienthanh <congphamtienthanh@gmail.com>
2022-04-09 21:44:26 +0700

    font eror

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/demo', function () {
+    return view('template.admin');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
