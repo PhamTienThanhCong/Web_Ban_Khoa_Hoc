2a490731ce938f2083a1c7305f4ee167a638e3c1
congphamtienthanh <congphamtienthanh@gmail.com>
2022-04-20 18:09:18 +0700

    fix bug name db

--- database/migrations/2022_04_20_035512_create_view_histories_table.php
+++ database/migrations/2022_04_20_035512_create_view_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLviewHistoriesTable extends Migration
+class CreateViewHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateLviewHistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('lview_histories', function (Blueprint $table) {
+        Schema::create('view_histories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('users_id')->constrained('users');
             $table->foreignId('courses_id')->constrained('courses');
@@ -29,6 +29,6 @@ class CreateLviewHistoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('lview_histories');
+        Schema::dropIfExists('view_histories');
     }
 }
